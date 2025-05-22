d45cb3b020d6bf04043ea5e421357210116ba672
KachmarAN <bilux.teplo@gmail.com>
2025-05-22 20:23:55 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import SmileApp from './components/SmileApp';
 import Footer from './components/Footer';
-import { ThemeProvider } from './contexts/ThemeContext';
-import { SmilesProvider } from './contexts/SmilesContext';
 
 function App() {
     return (
-        <ThemeProvider>
-            <SmilesProvider>
-                <div className="app">
-                    <Header />
-                    <SmileApp />
-                    <Footer />
-                </div>
-            </SmilesProvider>
-        </ThemeProvider>
+        <div className="app">
+            <Header/>
+            <SmileApp/>
+            <Footer/>
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { SmilesProvider } from './contexts/SmilesContext';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-root.render(<App/>);
+
+root.render(
+    <ThemeProvider>
+        <SmilesProvider>
+            <App />
+        </SmilesProvider>
+    </ThemeProvider>
+);
